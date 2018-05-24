e9e8c568dd8d378a20bb3ffeb8813ae21ac3b8f3
Son Ho <sonho@hearti.io>
2018-05-24 09:13:09 +0700

    Define pokemon entities

--- Pokedex/DetailViewController.swift
+++ Pokedex/DetailViewController.swift
@@ -11,11 +11,27 @@ import UIKit
 class DetailViewController: UIViewController {
     
     var pokemon: Pokemon!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var pokemonImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var pokedexIdLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var baseAttackLabel: UILabel!
+    @IBOutlet weak var evolutionLabel: UILabel!
+    @IBOutlet weak var currentEvolutionImage: UIImageView!
+    @IBOutlet weak var nextEvolutionImage: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print(pokemon.name.capitalized)
+        nameLabel.text = pokemon.name.capitalized
+        pokemonImage.image = UIImage(named: "\(pokemon.pokedexId)")
+        currentEvolutionImage.image = UIImage(named: "\(pokemon.pokedexId)")
     }
     
     @IBAction func backButtonPressed(_ sender: UIButton) {
--- Pokedex/Pokemon.swift
+++ Pokedex/Pokemon.swift
@@ -10,15 +10,77 @@ import Foundation
 
 class Pokemon {
     
-    fileprivate var _name: String!
-    fileprivate var _pokedexId: Int!
+    private var _name: String!
+    private var _pokedexId: Int!
+    private var _description: String!
+    private var _type: String!
+    private var _defense: Int!
+    private var _height: Int!
+    private var _weight: Int!
+    private var _baseAttack: Int!
+    private var _nextEvolution: String!
     
     var name: String {
+        if _name == nil {
+            _name = ""
+        }
         return _name
     }
     
     var pokedexId: Int {
+        if _pokedexId == nil {
+            _pokedexId = 0
+        }
         return _pokedexId
+    }
+    
+    var description: String {
+        if _description == nil {
+            _description = ""
+        }
+        return _description
+    }
+    
+    var type: String {
+        if _type == nil {
+            _type = ""
+        }
+        return _type
+    }
+    
+    var defense: Int {
+        if _defense == nil {
+            _defense = 0
+        }
+        return _defense
+    }
+    
+    var height: Int {
+        if _height == nil {
+            _height = 0
+        }
+        return _height
+    }
+    
+    var weight: Int {
+        if _weight == nil {
+            _weight = 0
+        }
+        return _weight
+    }
+    
+    var baseAttack: Int {
+        if _baseAttack == nil {
+            _baseAttack = 0
+        }
+        return _baseAttack
+    }
+    
+    var nextEvolution: String {
+        if _nextEvolution == nil {
+            _nextEvolution = ""
+        }
+        return _nextEvolution
     }
     
     init(name: String, pokedexId: Int) {
